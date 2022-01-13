22ada6bffb5360acfd69ccc42a016d16349f59cb
Wei Chen <PixelChen24@outlook.com>
2022-01-13 16:35:10 +0800

    Update AudioSet

--- README.md
+++ README.md
@@ -1,8 +1,23 @@
 ## Download Dataset of Music21 &amp; AudioSet &amp; VGGSound
-This repository is aimed to help you get datasets in field of music seperation.
+This repository is aimed to help you get datasets in field of music separation.
 * For `Nusic21` Dataset, run `Music21.py`. Downloaded files will be stored in `Dataset/Music21`
   * **Currently not available.**
+  
+    
+
+
+
 * For `AudioSet` Dataset, run `AudioSet.py`. Downloaded files will be stored in `Dataset/AudioSet`
-  * **Currently not available.**
+  * Files will be named in this format: `SerialNum`+`YoutubeID`+`.mp4`
+  * You may notice that filename does not contain label. Thats because the dataset itself is in a mess. A little  fragment may contain several type of music. To match each video with its `label index`(Starts with a '/' and a letter, for example  "/m/012xff"), refer to [CSV Source File](SourceFile/AudioSet/balanced_train_segments.csv). After you get the `label index`, you have to consult [ontology.json](SourceFile/AudioSet/ontology.json) or [this file](SourceFile/AudioSet/AudioSet.csv) for its real `label`
+
+
+
+
+
 * For `VGGSound` Dataset, run `VGGSound.py`. Downloaded files will be stored in `Dataset/VGGSound`
-  * Files wii be named in this format: `SerialNum+`+`label`+`.mp4`
+  * Files will be named in this format: `SerialNum+`+`label`+`.mp4`
+
+
+
+:warning:**The dataset is very very large. For each dataset, if you don't need every record of it, you can download part of it by assign `start` and `end` in each python file**
--- VGGSound.py
+++ VGGSound.py
@@ -41,25 +41,33 @@ for i in range(start, end):
         video = YouTube(video_url)  # not using proxy
         # video = YouTube(video_url, proxies=proxy_handler)  # using proxy
     except:
-        print("No." + str(i) + "  video:  **" + video_url + "**  not found.")
+        print("**No." + str(i) + "  video:  " + video_url + "  not found.**")
         fail += 1
         continue
 
-    video.streams.get_highest_resolution().download(output_path=save_path, filename=file_name)
+    # Download
+    try:
+        print("Trying to download %s......" % id)
+        video.streams.get_highest_resolution().download(output_path=save_path, filename=file_name)
+    except:
+        print("**No." + str(i) + "  video:  " + video_url + "  download failed.**")
+        fail += 1
+        continue
+
     file_path = save_path + file_name
-    # 拆分
+    # cut
     if next_id == id:  # 如果出现一个视频中后面将出现不同乐器，那么此次拆分区间为[start_seconds,next_start_seconds)
         source_file = VideoFileClip(file_path)
-        file = source_file.subclip(t_start=start_seconds, t_end=next_start_seconds)  # 裁剪视频
-        file.write_videofile(save_path + str(i) + "+" + label + ".mp4")
+        cut_file = source_file.subclip(t_start=start_seconds, t_end=next_start_seconds)  # 裁剪视频
+        cut_file.write_videofile(save_path + str(i) + "+" + label + ".mp4")
         source_file.reader.close()
     elif next_id != id or last:  # 如果一个视频里只出现一个乐器，或者这条记录是一个视频中最后一种乐器，那么裁剪范围为[start_seconds,)
         source_file = VideoFileClip(file_path)
-        file = source_file.subclip(t_start=start_seconds)
-        file.write_videofile(save_path + str(i) + "+" + label + ".mp4")
+        cut_file = source_file.subclip(t_start=start_seconds)
+        cut_file.write_videofile(save_path + str(i) + "+" + label + ".mp4")
         source_file.reader.close()
         os.remove(file_path)
     success += 1
-    print("Success!", id)
+    print("************No. %d success!************"%i)
 
 print("\n\nFinished. %d of %d success, %d failed." % (success, end - start, fail))
